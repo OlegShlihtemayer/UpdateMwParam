75d1112a59658365262ba59b938431011bd4b6f0
OlegShlihtemayer <oshlihtemayer@onevizion.com>
2024-11-07 12:38:27 +0500

    2.0

--- installation/030.Test.sql
+++ installation/030.Test.sql
@@ -1,4 +1,4 @@
 begin
-    dbms_output.put_line(util_module.get_module_param_val('test_repo', 'Token') || ' - encrypt');
-    dbms_output.put_line(util_module.get_decrypted_module_param_val('test_repo', 'Token') || ' - decrypt');
+    dbms_output.put_line(util_module.get_module_param_val('UpdateMwParam', 'Token') || ' - encrypt');
+    dbms_output.put_line(util_module.get_decrypted_module_param_val('UpdateMwParam', 'Token') || ' - decrypt');
 end;
